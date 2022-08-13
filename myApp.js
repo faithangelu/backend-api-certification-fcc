23f3033ed370a7fe9a91942792ada32c6f79de93
faithangelu <imfaithangelou@gmail.com>
2022-08-13 12:39:27 -0700

    * Serve Static Assets

--- myApp.js
+++ myApp.js
@@ -11,10 +11,10 @@ let app = express();
 
 // Serve an HTML File
 app.get("/", (req, res) => {    
-    res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(__dirname + "/views/index.html");    
 })
 
-
+app.use('/public', express.static(__dirname+"/public"));
 
 
 
