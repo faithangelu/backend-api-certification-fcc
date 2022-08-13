ee287d716f916c4e179a0294d47d00a533482e71
faithangelu <imfaithangelou@gmail.com>
2022-08-13 12:46:29 -0700

    * Serve Json on a Specific Route

--- myApp.js
+++ myApp.js
@@ -14,8 +14,13 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");    
 })
 
+// Serve Static Asstes
 app.use('/public', express.static(__dirname+"/public"));
 
+// Serve JSON on a Specific Route
+app.get('/json', (req,res) => {
+    res.json({ "message": "Hello json"})
+})
 
 
 
