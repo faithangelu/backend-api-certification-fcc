3cf4d84965b5d69bc3a27cade7ee7d4cdd82cc6d
faithangelu <imfaithangelou@gmail.com>
2022-08-13 11:31:54 -0700

    * update for Express Server

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.get('/data', function(req, res) {
+app.get('/', function(req, res) {
     res.send('Hello Express');
     res.end();
 })
