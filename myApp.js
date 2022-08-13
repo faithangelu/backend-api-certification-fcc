cd647a177d6aad09a762a8dbf509640a2677b180
faithangelu <imfaithangelou@gmail.com>
2022-08-13 12:18:54 -0700

    * Serving an HTML File

--- myApp.js
+++ myApp.js
@@ -1,10 +1,19 @@
 let express = require('express');
 let app = express();
 
-app.get("/", (req,res) => { 
-   res.send("Hello Express");
-})
+// Meet the Node Console 
+// console.log("Hello World")
 
+// Start a working Express Server
+// app.get("/", (req,res) => { 
+//    res.send("Hello Express");
+// })
+
+// Serve an HTML File
+app.get("/views/index.html", (req, res) => {
+    let absolutePath = __dirname + relativepath/File.ext
+    res.sendFile(absolutePath);
+})
 
 
 
