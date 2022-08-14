d186cb993e2f89263e7e78dbb0bc769a494d4d09
faithangelu <imfaithangelou@gmail.com>
2022-08-14 14:46:13 -0700

    * removing comments

--- myApp.js
+++ myApp.js
@@ -4,20 +4,16 @@ let dotenv = require('dotenv')
 
 dotenv.config();
 
-// Meet the Node Console 
 // console.log("Hello World")
 
-// Start a working Express Server
 // app.get("/", (req,res) => { 
 //    res.send("Hello Express");
 // })
 
-Serve an HTML File
 app.get("/", (req, res) => {    
     res.sendFile(__dirname + "/views/index.html");    
 })
 
-// Serve Static Asstes
 app.use('/public', express.static(__dirname+"/public"));
 
 // // Serve JSON on a Specific Route
