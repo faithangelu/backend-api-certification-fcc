ff4f9ee9bed8114b91ef19b63260bb1d2176c16a
faithangelu <imfaithangelou@gmail.com>
2022-08-14 12:36:08 -0700

    * updates root logger

--- myApp.js
+++ myApp.js
@@ -13,25 +13,29 @@ dotenv.config();
 // })
 
 // Serve an HTML File
-app.get("/", (req, res) => {    
-    res.sendFile(__dirname + "/views/index.html");    
+// app.get("/", (req, res) => {    
+//     res.sendFile(__dirname + "/views/index.html");    
+// })
+
+// // Serve Static Asstes
+// app.use('/public', express.static(__dirname+"/public"));
+
+// // Serve JSON on a Specific Route
+// app.get('/json', (req,res) => {
+//     if (process.env.MESSAGE_STYLE === "uppercase") {
+//         res.send({ message: "HELLO JSON"})
+//         // console.log(process.env.MESSAGE_STYLE)
+//     } else {
+//         // console.log('Not working')
+//         res.send({message: "Hello json"})
+//     } 
+// })
+
+
+app.use((req, res, next) => {
+   console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next()
 })
-
-// Serve Static Asstes
-app.use('/public', express.static(__dirname+"/public"));
-
-// Serve JSON on a Specific Route
-app.get('/json', (req,res) => {
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.send({ message: "HELLO JSON"})
-        // console.log(process.env.MESSAGE_STYLE)
-    } else {
-        // console.log('Not working')
-        res.send({message: "Hello json"})
-    } 
-})
-
-
 
 // app.listen(3000, function() {
 //     console.log("Serving 3000")
